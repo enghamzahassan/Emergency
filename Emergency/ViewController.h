741088e9d282bcf6d4bd6f07061837fa8de6c346
GazaSkyGeeks <gazaskygeeks@camd-cmts-14-tenge130.network.virginmedia.net>
2016-05-06 22:45:51 +0300

    Initial Commit

---
+++ Emergency/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Emergency
+//
+//  Created by GazaSkyGeeks on 5/6/16.
+//  Copyright © 2016 hamza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
